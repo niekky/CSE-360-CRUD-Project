d13b445fcd8e985cae4e1e41e72126c2bab9c5f8
niekky <trungkiencfc@gmail.com>
2025-02-11 20:17:42 -0700

    Added Question and Questions classes

---
+++ FoundationCode/src/application/Question.java
@@ -0,0 +1,44 @@
+package application;
+
+public class Question {
+	private int id;
+	// Variable Declaration
+	private String ques_content;
+	private String username;
+	private String time_created;
+	private boolean resolved;
+	
+	// Constructor
+	public Question(int id, String ques_content, String username, String time_created, boolean resolved) {
+		this.id = id;
+		this.ques_content = ques_content;
+		this.username = username;
+		this.time_created = time_created;
+		this.resolved = resolved;			// 1: Resolved, 0: Unresolved
+	}
+
+	// API Functions
+	public String getQuestion() {
+		return this.ques_content;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getTimeCreated() {
+		return this.time_created;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public boolean getResolved() {
+		return this.resolved;
+	}
+	
+	public void setResolved(boolean resolved_status) {
+		this.resolved = resolved_status;
+	}
+}
---
+++ FoundationCode/src/application/Questions.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.ArrayList;
+
+import databasePart1.DatabaseHelper;
+
+public class Questions {
+	// Variable Declaration
+	private ArrayList<Question> question_bank;
+	
+	// Constructor
+	public Questions(DatabaseHelper database) {
+		ArrayList<Question> rs = new ArrayList<Question>();
+		
+		// Request API to retrieve questions
+		
+		// Iterate through the polled data
+		
+		this.question_bank = rs;
+	}
+	
+	// API Functions
+	public void fetchQuestions(){
+		ArrayList<Question> rs = new ArrayList<Question>();
+		
+		// Request API to retrieve questions
+		
+		// Iterate through the polled data
+		
+		this.question_bank = rs;
+	}
+	
+	public ArrayList<Question> getAllQuestions(){
+		return this.question_bank;
+	}
+	
+	public void filterQuestionBank() {
+		ArrayList<Question> rs = new ArrayList<Question>();
+		
+		this.question_bank = rs;
+	}
+}
